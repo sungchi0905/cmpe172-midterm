ea9e82fe677dc64d2a10608e9f5aad3b77d1a75d
Sung Chi <sungchi91@gmail.com>
2016-10-21 22:19:06 -0700

    added real time API calls

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const request = require('superagent');
 const colors = require('colors');
 const repl = require('repl');
 const fs = require('fs');
-const csv = require('csv');
+const csv = require('fast-csv');
 const _ = require('underscore');
 
 var HEADERS_DEFAULT = ["Timestamp", "Order", "Detail"];
@@ -16,129 +16,131 @@ var listCurrencies =function () {
      if (err || !res.ok) {
        	console.log('Oh no! error');
      } else {
-       	//console.log('yay got ' + JSON.stringify(res.body));
 		var currencyList = res.body;
-		listExchangeRates(currencyList);
+		userPrompt(currencyList);
      }
    });
 };
 
-var listExchangeRates =function (currencyList) {  
+var listExchangeRates =function (callback) {  
   request
    .get('https://api.coinbase.com/v1/currencies/exchange_rates')
    .end(function(err, res){
      if (err || !res.ok) {
 		 console.log('Oh no! error');
      } else {
-		 //console.log('yay got ' + JSON.stringify(res.body));
 		 var exchangeRatesList = res.body;
-		 userPrompt(currencyList,exchangeRatesList);
+		 var exchangeRate = _.propertyOf(exchangeRatesList)("btc_to_usd");
+		 callback(exchangeRatesList);
      }
    });
 };
 
 var csv2console = function(csvfile, callback) {
-   var parser = csv.parse(); //create the parser
-	var outrow = "";
-    parser.on('readable',function(){
-    while(row = parser.read()) {
-        var timestamp = row[0];
-        var order = row[1];
-        var type = row[2];
-        outrow = outrow+timestamp+" : "+order+" : "+type+"\n";
-     }
-    });
-	parser.on('end', function(){
-		callback(outrow);
+	listExchangeRates(function(exchangeRatesList){
+		var exchangeRate = _.propertyOf(exchangeRatesList)("btc_to_usd");
+		console.log("current BTC/USD: "+exchangeRate);
+		console.log(" === CURRENT ORDERS ===");	
+		var outrow = "";
+		
+		var stream = fs.createReadStream(csvfile);
+
+		csv
+		 .fromStream(stream)
+		 .on("data", function(data){
+			 outrow += data[0]+" : "+data[1]+" : "+data[2]+"\n";
+		 })
+		 .on("end", function(){
+			 callback(outrow);
+		 });
 	});
-   parser.on('error', function(err){
-     console.log(err.message);
-   });
-	fs.createReadStream(csvfile).pipe(parser);//Open file as stream and pipe it to parser
 };
 
 
-var buildfn = function(csvfile, orders, callback) {
-	//console.log(orders);
-	//fs.writeFileSync(csvfile, '"Wed Oct 05 2016 22:09:40 GMT+0000 (UTC)","BUY 10","UNFILLED"\n"Mon Oct 05 2016 22:09:40 GMT+0000 (UTC)","SELL 25","UNFILLED"');
-	fs.writeFileSync(csvfile, orders);
+var buildfn = function(csvfile, orderCmd, callback) {
+	fs.writeFileSync(csvfile, orderCmd);
     csv2console(csvfile, callback);
 };
 
-var orders = function(csvfile, callback) {
-    csvfile = csvfile || CSVFILE_DEFAULT;
-	orders = ORDERS;
-	buildfn(csvfile,orders,callback);
+var orders = function(callback) {
+    csvfile = CSVFILE_DEFAULT;
+	orderCmd = ORDERS;
+	buildfn(csvfile,orderCmd,callback);
 };
 
 
-var buy = function(currencyList,exchangeRatesList,arg1,arg2,callback) {
-	if(arg2 != null || arg2 != undefined){
-		if(!_.contains(_.flatten(currencyList),arg2.toUpperCase())){
-		callback("No known exchange rate for BTC/"+arg2.toUpperCase()+". Order failed");
-		return;
-		}else{
-			var exchangeRate = _.propertyOf(exchangeRatesList)("btc_to_"+arg2);
-			var count = arg1 / exchangeRate;
+var buy = function(currencyList,arg1,arg2,callback) {
+	listExchangeRates(function(exchangeRatesList){
 			
-			result = "Order to BUY "+arg1+" "+arg2.toUpperCase()+" worth of BTC queued @ "+exchangeRate+" BTC/USD ("+count+" BTC)";
-		} 
-	}else{
-		result = "Order to BUY "+arg1+" BTC queued" ;
-	}	
-	
-	var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-	var dayNames = ["Sun","Mon","Tues","Wed","Thu","Fri","Sat"];
-	var today = new Date(); 
+		if(arg2 != null || arg2 != undefined){
+			if(!_.contains(_.flatten(currencyList),arg2.toUpperCase())){
+			callback("No known exchange rate for BTC/"+arg2.toUpperCase()+". Order failed");
+			return;
+			}else{
+				var exchangeRate = _.propertyOf(exchangeRatesList)("btc_to_"+arg2.toLowerCase());
+				var count = arg1 / exchangeRate;
 
-	var datetime = dayNames[today.getUTCDay()]+" "+monthNames[today.getUTCMonth()]+ " " + today.getUTCDate()+" "+today.getFullYear()+" "+today.getUTCHours()+":"+today.getUTCMinutes()+":"+today.getUTCSeconds()+" GMT+0000 (UTC)";
-
-	var order = "BUY "+arg1;
-	if (arg2!= undefined){
-		order = order+arg2.toUpperCase();
-	}else{
-		order = order+"BTC";
-	}
-	var type = "UNFILLED";
-	ORDERS = ORDERS + '"'+datetime+'",'+'"'+order+'",'+'"'+type+'"\n';
-	callback(result);
-};
-
-
-var sell = function(currencyList,exchangeRatesList,arg1,arg2,callback) {
-	if(arg2 != null || arg2 != undefined){
-		if(!_.contains(_.flatten(currencyList),arg2.toUpperCase())){
-		callback("No known exchange rate for BTC/"+arg2.toUpperCase()+". Order failed");
-		return;
+				result = "Order to BUY "+arg1+" "+arg2.toUpperCase()+" worth of BTC queued @ "+exchangeRate+" BTC/USD ("+count+" BTC)";
+			} 
 		}else{
-			var exchangeRate = _.propertyOf(exchangeRatesList)("btc_to_"+arg2);
-			var count = arg1 / exchangeRate;
-			
-			result = "Order to SELL "+arg1+" "+arg2.toUpperCase()+" worth of BTC queued @ "+exchangeRate+" BTC/USD ("+count+" BTC)";
-		} 
-	}else{
-		result = "Order to SELL "+arg1+" BTC queued" ;
-	}	
-	
-	var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-	var dayNames = ["Sun","Mon","Tues","Wed","Thu","Fri","Sat"];
-	var today = new Date(); 
+			result = "Order to BUY "+arg1+" BTC queued" ;
+		}	
 
-	var datetime = dayNames[today.getUTCDay()]+" "+monthNames[today.getUTCMonth()]+ " " + today.getUTCDate()+" "+today.getFullYear()+" "+today.getUTCHours()+":"+today.getUTCMinutes()+":"+today.getUTCSeconds()+" GMT+0000 (UTC)";
+		var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+		var dayNames = ["Sun","Mon","Tues","Wed","Thu","Fri","Sat"];
+		var today = new Date(); 
 
-	var order = "SELL "+arg1;
-	if (arg2!= undefined){
-		order = order+arg2.toUpperCase();
-	}else{
-		order = order+"BTC";
-	}
-	var type = "UNFILLED";
-	ORDERS = ORDERS + '"'+datetime+'",'+'"'+order+'",'+'"'+type+'"\n';
-	callback(result);
+		var datetime = dayNames[today.getUTCDay()]+" "+monthNames[today.getUTCMonth()]+ " " + today.getUTCDate()+" "+today.getFullYear()+" "+today.getUTCHours()+":"+today.getUTCMinutes()+":"+today.getUTCSeconds()+" GMT+0000 (UTC)";
+
+		var order = "BUY "+arg1;
+		if (arg2!= undefined){
+			order = order+arg2.toUpperCase();
+		}else{
+			order = order+"BTC";
+		}
+		var type = "UNFILLED";
+		ORDERS = ORDERS + '"'+datetime+'",'+'"'+order+'",'+'"'+type+'"\n';
+		callback(result);
+	});
 };
 
 
-var userInputController = function(cmd,currencyList,exchangeRatesList,cb){
+var sell = function(currencyList,arg1,arg2,callback) {
+	listExchangeRates(function(exchangeRatesList){
+		if(arg2 != null || arg2 != undefined){
+			if(!_.contains(_.flatten(currencyList),arg2.toUpperCase())){
+			callback("No known exchange rate for BTC/"+arg2.toUpperCase()+". Order failed");
+			return;
+			}else{
+				var exchangeRate = _.propertyOf(exchangeRatesList)("btc_to_"+arg2.toLowerCase());
+				var count = arg1 / exchangeRate;
+
+				result = "Order to SELL "+arg1+" "+arg2.toUpperCase()+" worth of BTC queued @ "+exchangeRate+" BTC/USD ("+count+" BTC)";
+			} 
+		}else{
+			result = "Order to SELL "+arg1+" BTC queued" ;
+		}	
+
+		var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+		var dayNames = ["Sun","Mon","Tues","Wed","Thu","Fri","Sat"];
+		var today = new Date(); 
+
+		var datetime = dayNames[today.getUTCDay()]+" "+monthNames[today.getUTCMonth()]+ " " + today.getUTCDate()+" "+today.getFullYear()+" "+today.getUTCHours()+":"+today.getUTCMinutes()+":"+today.getUTCSeconds()+" GMT+0000 (UTC)";
+
+		var order = "SELL "+arg1;
+		if (arg2!= undefined){
+			order = order+arg2.toUpperCase();
+		}else{
+			order = order+"BTC";
+		}
+		var type = "UNFILLED";
+		ORDERS = ORDERS + '"'+datetime+'",'+'"'+order+'",'+'"'+type+'"\n';
+		callback(result);
+	});
+};
+
+
+var userInputController = function(cmd,currencyList,cb){
 	var cmdArr = cmd.replace(/(\r\n|\n|\r)/gm,"").split(" ");
 
 	switch(cmdArr[0].toUpperCase()) {
@@ -154,7 +156,7 @@ var userInputController = function(cmd,currencyList,exchangeRatesList,cb){
 				cb("Not valid amount value");
 				break;
 			}
-			buy(currencyList,exchangeRatesList,cmdArr[1],cmdArr[2],function(res){	
+			buy(currencyList,cmdArr[1],cmdArr[2],function(res){	
 				cb(res);
 			});	
 			break;
@@ -170,26 +172,21 @@ var userInputController = function(cmd,currencyList,exchangeRatesList,cb){
 				cb("Not valid amount value");
 				break;
 			}
-			sell(currencyList,exchangeRatesList,cmdArr[1],cmdArr[2],function(res){
+			sell(currencyList,cmdArr[1],cmdArr[2],function(res){
 				cb(res);
 			});	
 			break;
 		case "ORDERS":
-			orders(null,function(res){				
-				var exchangeRate = _.propertyOf(exchangeRatesList)("btc_to_usd");
-				console.log("current BTC/USD: "+exchangeRate);
-				console.log(" === CURRENT ORDERS ===");	
+			orders(function(res){
 				cb(res);
-			});			
+			});				
 			break;
 		default:
 			cb( "Not supported Command. Please Try Again");
 			
 	}
 }
-var userPrompt = function (currencyList,exchangeRatesList) {
-	//console.log(JSON.stringify(currencyList));
-	//console.log(JSON.stringify(exchangeRatesList));
+var userPrompt = function (currencyList) {
 	console.log("Welcome to Bitcoin Exchange.".bold);
 	console.log("Command-Line console for exchanging Bitcoins\n");	
 	console.log("	 Syntax: BUY <amount> [currency]");
@@ -199,7 +196,7 @@ var userPrompt = function (currencyList,exchangeRatesList) {
 	repl.start({
     prompt: 'coinbase> ',
     eval: function (cmd, context, filename, callback) {		
-		userInputController(cmd,currencyList,exchangeRatesList,function(data){
+		userInputController(cmd,currencyList,function(data){
 			callback(null,data); 
 		});
     },
